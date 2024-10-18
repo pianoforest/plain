14c6e558db3daef0755a06205e2eb0a33c5cb87d
pianoforest <pianoforest@mju.ac.kr>
2024-10-19 00:57:14 +0900

    http://192.168.0.9:3000

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import { BrowserRouter } from 'react-router-dom';
-import './App.css'; 
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <header>
-        <h2>Hello World</h2>
+    <div style={{
+      backgroundImage: 'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZ17r745DzK4Cbhznqy1ggKjGqr3_ket04IA&s")'
+    }}>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+            <h2>안녕하세요!</h2>
       </header>
     </div>
-  )
-};
+  );
+}
 
 export default App;
